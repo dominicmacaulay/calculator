bc62b421af8000fd651e6367819d6e46fb451b55
dominicmacaulay <dominic.62502@gmail.com>
2023-12-09 01:13:08 -0500

    Fixed single digit error
    The user was only able to enter single digit inputs. A new update has
    solved this issue so that the user can input multiple digits before
    entering them as a complete input. This is currently the last
    mechanical update.

--- calc.js
+++ calc.js
@@ -9,6 +9,9 @@ const operators = document.querySelectorAll('.operators');
 const submitBtn = document.querySelector('#submit');
 const clearBtn = document.querySelector('#clear');
 
+// maintains whether the first number is being manipulated by the user
+isOnNum1 = true;
+
 // assign event listeners to the buttons that, when clicked, call their functions with
 // each buttons value as a parameter
 nums.forEach((elem) => {
@@ -32,7 +35,8 @@ clear();
 
 // called by operator: checks if let operator has value and calls
 // corresponding operator function and replaces let operator
-// with new operator value
+// with new operator value. If let operator has value, switch
+// to manipulating num2.
 function operatorCall(value){
     if(operator !== null){
         if(operator == 1){
@@ -44,21 +48,32 @@ function operatorCall(value){
         } else if(operator == 4){
             divide();
         }
+    } else{
+        isOnNum1 = false;
     }
     operator = value;
 }
 
-// called by number buttons: checks if let num1 is null. If it is,
-// replace it with button value. If not, then replace let num2
-// with button value.
+// called by number buttons: checks if let num1 is being manipulated.
+// If it is,replace it with button value until it is no longer being
+// manipulated. If not, then replace let num2 with button value.
+// Keep adding button value to nums as a string until they are null
+// again.
 function buttonPress(value){
-    text.textContent = value;
-    if(num1 == null){
-        num1 = value;
-        console.log(num1)
+    if(isOnNum1 == true){
+        if(num1 == null){
+            num1 = value;
+        } else {
+            num1 += value;
+        }
+        text.textContent = num1;
     } else{
-        num2 = value;
-        console.log(num2)
+        if(num2 == null){
+            num2 = value;
+        } else{
+            num2 += value;
+        }
+        text.textContent = num2;
     }
 }
 
@@ -80,6 +95,7 @@ function clear(condition){
     interim = null;
     operator = null;
     solution = null;
+    isOnNum1 = true;
     if (!condition){
         text.textContent = "";
     }
@@ -89,18 +105,18 @@ function clear(condition){
 // into the solution variable. It then makes num1 equal to solution
 function add(){
     interim = parseFloat(num1) + parseFloat(num2);
-    num1 = interim;
+    setValues();
     console.log('interim: ' + interim);
 }
 function subtract(){
     interim = parseFloat(num1) - parseFloat(num2);
-    num1 = interim;
+    setValues();
     console.log('interim: ' + interim);
 }
 function multiply(){
     if (num1 != 0 && num2 != 0){
         interim = parseFloat(num1) * parseFloat(num2);
-        num1 = interim;
+        setValues();
         console.log('interim: ' + interim);
     } else{
         clear();
@@ -109,6 +125,12 @@ function multiply(){
 }
 function divide(){
     interim = parseFloat(num1) / parseFloat(num2);
-    num1 = interim;
+    setValues();
     console.log('interim: ' + interim);
+}
+// make num1 equal new value from function and clear num2 to start
+// a new line.
+function setValues(){
+    num1 = interim;
+    num2 = null;
 }
